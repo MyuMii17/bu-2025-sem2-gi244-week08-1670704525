7139f13e403a2770a98e94048850605e94fb9bd2
MyuMii17 <mewgamer17@gmail.com>
2026-03-12 14:02:20 +0700

    Revert "V1"

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -69,18 +69,6 @@ public class CharacterController : MonoBehaviour
         if (anim != null)
         {
             float speed = Mathf.Abs(moveInput.y);
-            anim.SetFloat("Speed_f",speed);
-            anim.SetBool("Crouch_b",isCrouching);
-            if (isUsingHandgun)
-            {
-                anim.SetInteger("WeaponType_int",1);
-                handgun.SetActive(true);
-            }
-            else
-            {
-                anim.SetInteger("WeaponType_int",0);
-                handgun.SetActive(false);
-            }
         }
     }
 }
